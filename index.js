930b46ad67192bee15b5077925e64768983cfa3e
ABANGA SAMUEL <psalmpally@gmail.com>
2023-01-21 13:45:40 +0100

    Added some functions to index.js and also updated index.css

---
+++ index.js
@@ -0,0 +1,53 @@
+let addEl = 0
+let addEl2 = 0
+let addEl3 = 0
+
+
+
+function add1() {
+    addEl += 1
+    console.log(addEl)
+    scores1 = document.getElementById("resultH")
+    scores1.textContent = addEl
+}
+
+
+function add2() {
+    
+    addEl2 += 2
+    console.log(addEl2)
+    scores1 = document.getElementById("resultH")
+    scores1.textContent = addEl2
+}
+
+function add3() {
+    addEl3 += 3
+    console.log(addEl3)
+    scores1 = document.getElementById("resultH")
+    scores1.textContent = addEl3
+}
+
+function addG1() {
+    addEl += 1
+    console.log(addEl)
+    scores2 = document.getElementById("resultG")
+    scores2.textContent = addEl
+}
+
+
+function addG2() {
+    
+    addEl2 += 2
+    console.log(addEl2)
+    scores2 = document.getElementById("resultG")
+    scores2.textContent = addEl2
+    
+}
+
+function addG3() {
+    addEl3 += 3
+    console.log(addEl3)
+    scores2 = document.getElementById("resultG")
+    scores2.textContent = addEl3
+
+}
